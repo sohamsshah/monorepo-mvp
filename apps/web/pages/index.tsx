b6888b5f7b3a90012eedd1ad1b86d1d21c3bbe2a
sohamsshah <sohamshah456@gmail.com>
2022-01-14 16:47:40 +0530

    first commit

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>I am the Student Side</h1>
       <Button />
     </div>
   );
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button>I am the button</button>;
 };
